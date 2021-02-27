a79425a3d172780a22e0b60a6894f5eb50e16a44
Muhamad Dendi Purwanto <73510776+muhamaddendipurwanto@users.noreply.github.com>
2021-02-27 20:46:38 +0700

    Source Code Pemograman Pascal

---
+++ konversi_suhu_fahrenthiet_ke_celcius.pas
@@ -0,0 +1,22 @@
+{Nama           : Muhamad Dendi Purwanto    }
+{NPM            : 20.14.1.0001              }
+{Nama Program   : konversi_suhu_fahrenthiet_ke_celcius.pas  }
+
+program konversi_suhu_fahrenthiet_ke_celcius;
+uses crt;
+
+var
+suhu : real;
+
+begin
+writeln('Program Konversi Suhu Fahrentheit - celcius');
+writeln('===========================================');
+
+
+
+
+
+
+readln;
+end.
+
